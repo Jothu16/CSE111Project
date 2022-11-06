017bf30c83bdea13b08a1dc7798cde97e4501383
Harjot Grewal <61030016+Jothu16@users.noreply.github.com>
2022-11-06 13:03:33 -0800

    Python&SQL &newdb. INSERT &DELETE. Needs Update
    created Python, SQL, and new db to work together. Has INSERT and DELETE function. Needs Update

---
+++ Python.py
@@ -0,0 +1,108 @@
+import sqlite3
+
+db = sqlite3.connect("newdb.db")
+cursor = db.cursor()
+
+# cursor.execute("""CREATE TABLE Function_Table(
+#     AQ_Key INTEGER PRIMARY KEY,
+#     Date TEXT,
+#     Country TEXT,
+#     Status TEXT,
+#     AQI_Value TEXT
+#     )""")
+
+# Add Function_Table to the table and commit these changes to the database
+Function_Table = (1,'2022-07-21','Albania','Good',14)
+cursor.execute("INSERT OR REPLACE INTO Function_Table VALUES(?,?,?,?,?)", Function_Table)
+db.commit()
+
+Function_Table = (2,'2022-07-21','Algeria','Moderate',65)
+cursor.execute("INSERT OR REPLACE INTO Function_Table VALUES(?,?,?,?,?)", Function_Table)
+db.commit()
+
+Function_Table = (3,'2022-07-21','Andorra','Moderate',55)
+cursor.execute("INSERT OR REPLACE INTO Function_Table VALUES(?,?,?,?,?)", Function_Table)
+db.commit()
+
+Function_Table = (4, '2022-07-21','Angola','Unhealthy for Sensitive Groups',113)
+cursor.execute("INSERT OR REPLACE INTO Function_Table VALUES(?,?,?,?,?)", Function_Table)
+db.commit()
+
+Function_Table = (5, '2022-07-21','Argentina','Moderate',63)
+cursor.execute("INSERT OR REPLACE INTO Function_Table VALUES(?,?,?,?,?)", Function_Table)
+db.commit()
+
+Function_Table = (6, '2022-07-21','Armenia','Moderate',76)
+cursor.execute("INSERT OR REPLACE INTO Function_Table VALUES(?,?,?,?,?)", Function_Table)
+db.commit()
+
+Function_Table = (7, '2022-07-21','Australia','Moderate',56)
+cursor.execute("INSERT OR REPLACE INTO Function_Table VALUES(?,?,?,?,?)", Function_Table)
+db.commit()
+
+Function_Table = (8, '2022-07-21','Austria','Good',45)
+cursor.execute("INSERT OR REPLACE INTO Function_Table VALUES(?,?,?,?,?)", Function_Table)
+db.commit()
+
+Function_Table = (9, '2022-07-21','Azerbaijan','Good',12)
+cursor.execute("INSERT OR REPLACE INTO Function_Table VALUES(?,?,?,?,?)", Function_Table)
+db.commit()
+
+Function_Table = (10, '2022-07-21','Bahrain','Unhealthy',165)
+cursor.execute("INSERT OR REPLACE INTO Function_Table VALUES(?,?,?,?,?)", Function_Table)
+db.commit()
+
+Function_Table = (11,'2022-07-21','Bangladesh','Unhealthy for Sensitive Groups',141)
+cursor.execute("INSERT OR REPLACE INTO Function_Table VALUES(?,?,?,?,?)", Function_Table)
+db.commit()
+
+Function_Table = (12, '2022-07-21','Belarus','Good',13)
+cursor.execute("INSERT OR REPLACE INTO Function_Table VALUES(?,?,?,?,?)", Function_Table)
+db.commit()
+
+Function_Table = (13, '2022-07-21','Belgium','Moderate',61)
+cursor.execute("INSERT OR REPLACE INTO Function_Table VALUES(?,?,?,?,?)", Function_Table)
+db.commit()
+
+Function_Table = (14, '2022-07-21','Belize','Good',28)
+cursor.execute("INSERT OR REPLACE INTO Function_Table VALUES(?,?,?,?,?)", Function_Table)
+db.commit()
+
+Function_Table = (15, '2022-07-21','Bermuda','Good',12)
+cursor.execute("INSERT OR REPLACE INTO Function_Table VALUES(?,?,?,?,?)", Function_Table)
+db.commit()
+
+Function_Table = (16, '2022-07-21','Bolivia','Good',9)
+cursor.execute("INSERT OR REPLACE INTO Function_Table VALUES(?,?,?,?,?)", Function_Table)
+db.commit()
+
+Function_Table = (17, '2022-07-21','Bosnia and Herzegovina','Moderate',58)
+cursor.execute("INSERT OR REPLACE INTO Function_Table VALUES(?,?,?,?,?)", Function_Table)
+db.commit()
+
+Function_Table = (18, '2022-07-21','Brazil','Moderate',67)
+cursor.execute("INSERT OR REPLACE INTO Function_Table VALUES(?,?,?,?,?)", Function_Table)
+db.commit()
+
+Function_Table = (19, '2022-07-21','Brunei','Good',15)
+cursor.execute("INSERT OR REPLACE INTO Function_Table VALUES(?,?,?,?,?)", Function_Table)
+db.commit()
+
+Function_Table = (20, '2022-07-21','Bulgaria','Good',28)
+cursor.execute("INSERT OR REPLACE INTO Function_Table VALUES(?,?,?,?,?)", Function_Table)
+db.commit()
+
+Function_Table = (21, '2022-07-21','Burkina Faso','Unhealthy for Sensitive Groups',118)
+cursor.execute("INSERT OR REPLACE INTO Function_Table VALUES(?,?,?,?,?)", Function_Table)
+db.commit()
+
+#Delete data(needs fixing or maybe put into separate file)
+cursor.execute("DELETE FROM Function_Table WHERE AQ_Key = 1000") #change number to which key you want to delete (leave at 1000 to not delete anything)
+
+#Select statment get data from Function_Table
+cursor.execute("SELECT * FROM Function_Table")
+Function_Table = cursor.fetchall()
+
+# Loops through prints each row
+for row in Function_Table:
+    print(row)
