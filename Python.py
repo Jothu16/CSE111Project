278323feda2142da51e45efce90f32ea51f9a11b
Eric Li <49456428+ericli33@users.noreply.github.com>
2022-11-08 13:33:46 -0800

    20 statements hopefully

--- Python.py
+++ Python.py
@@ -98,7 +98,7 @@ Status = (5, 'Very Unhealthy', 201, 300)
 cursor.execute("INSERT INTO Status VALUES(?,?,?,?)", Status)
 db.commit()
 
-Status = (6, 'Very Unhealthy', 301, 999999)
+Status = (6, 'Maroon', 301, 999999)
 cursor.execute("INSERT INTO Status VALUES(?,?,?,?)", Status)
 db.commit()
 
@@ -450,7 +450,7 @@ History = (21, '2022-07-21', 118, 21)
 cursor.execute("INSERT INTO History VALUES(?,?,?,?)", History)
 db.commit()
 
-#statement count: 13
+#statement count: 20
 
 #count amount of history entries and user entries
 cursor.execute("SELECT COUNT(History.History_Key) FROM History UNION SELECT COUNT(Users.User_Key) FROM Users")
@@ -463,36 +463,41 @@ cursor.execute("SELECT COUNT(Capital_City.City_Key) FROM Capital_City")
 max_country_key = max_city_key = max_aq_key = cursor.fetchone()[0]
 #print(max_country_key, max_city_key, max_aq_key)
 
-#get userkey
+#get current userkey
 name = ('Bronya', 'Zaychik')
 cursor.execute("SELECT Users.User_Key FROM Users WHERE Users.First_Name = ? AND Users.Last_Name = ?", name)
 user_key = cursor.fetchone()[0]
 #print(user_key)
 
-#add new user
+#add new user - copy paste this and change variables to make work with other tables
 max_user_key += 1
 new_user = (max_user_key, 'Florin', 'Rusu')
 cursor.execute("INSERT INTO Users VALUES(?,?,?)", new_user)
 db.commit()
 
-#Delete data relating to a city
+#Delete data relating to a city - copy paste this segment and change some names to make work with other tables
 selected_city = 'Ouagadougou'
 cursor.execute("SELECT Capital_City.City_Key FROM Capital_City WHERE Capital_City.Name = ?", [selected_city])
 output = cursor.fetchone()[0]
 cursor.execute("DELETE FROM Current_AQ_Info WHERE Current_AQ_Info.City_Key = ?", [output])
 max_aq_key -= 1
+db.commit()
 
 #Update file - date = new_date where date = old_date
 new_date = '2022-07-22'
 old_date = '2022-07-21'
 dates = (new_date, old_date)
 cursor.execute("UPDATE Current_AQ_Info SET Date = ? WHERE Date = ?", dates)
+db.commit()
 
 #update aqi value of a city - records user who did - check valid data later
-city_key = 1
+selected_city = 'Tirana'
+cursor.execute("SELECT Capital_City.City_Key FROM Capital_City WHERE Capital_City.Name = ?", [selected_city])
+city_key = cursor.fetchone()[0]
 new_AQI_value = 69
 updated_values = (new_AQI_value, user_key, city_key)
 cursor.execute("UPDATE Current_AQ_Info SET AQI_Value = ?, User_Key = ? WHERE City_Key = ?", updated_values)
+db.commit()
 #update history with new entry
 max_hist_key += 1
 new_history = (max_hist_key, new_date, new_AQI_value, city_key)
@@ -502,10 +507,14 @@ db.commit()
 #add new entry to current aq database
 max_aq_key += 1
 city_key = 21
-AQI_value = 420
+AQI_value = 120
 date = '2022-07-22'
 new_entry = (max_aq_key, date, AQI_value, city_key, user_key)
 cursor.execute("INSERT INTO Current_AQ_Info VALUES (?,?,?,?,?)", new_entry)
+db.commit()
+max_hist_key += 1
+new_history = (max_hist_key, date, AQI_value, city_key)
+cursor.execute("INSERT INTO History VALUES(?,?,?,?)", new_history)
 db.commit()
 
 #Select statment get data from Current_AQ_Info
@@ -530,7 +539,7 @@ cursor.execute("""SELECT best_con.Name, MIN(best_aq.AQI_Value)
                     FROM Current_AQ_Info best_aq
                     INNER JOIN Capital_City best_city ON best_aq.City_Key = best_city.City_Key
                     INNER JOIN Country best_con ON best_city.Country_Key = best_con.Country_Key
-               UNION
+                  UNION
                   SELECT worst_con.Name, MAX(worst_aq.AQI_Value)
                     FROM Current_AQ_Info worst_aq
                     INNER JOIN Capital_City worst_city ON worst_aq.City_Key = worst_city.City_Key
@@ -572,8 +581,93 @@ cursor.execute("""SELECT Continent.Name, COUNT(Country.Name) FROM Current_AQ_Inf
                          AS good_cities ON good_cities.good_ones = Capital_City.Name
                     GROUP BY Continent.Name""")
 output = cursor.fetchall()
-for row in output:
-    print(row)
+#for row in output:
+    #print(row)
+
+# Count the amount of entries for each status description
+cursor.execute("""SELECT Status.Description, COUNT(Status.Description)
+                    FROM Current_AQ_Info, Status
+                    INNER JOIN Capital_City ON Capital_City.City_Key = Current_AQ_Info.City_Key
+                    WHERE Current_AQ_Info.AQI_Value >= Status.Threshold_Lower
+                    AND Current_AQ_Info.AQI_Value <= Status.Threshold_Upper
+                    GROUP BY Status.Description
+                    ORDER BY COUNT(Status.Description) DESC""")
+output = cursor.fetchall()
+#for row in output:
+    #print(row)
+
+#Print the history of a city
+selected_city = 'Tirana'
+cursor.execute("""SELECT * FROM History WHERE History.City_key = (SELECT Capital_City.City_Key FROM Capital_City
+                                                                      WHERE Capital_City.Name = ?)""", [selected_city])
+output = cursor.fetchall()
+#for row in output:
+    #print(row)
+
+#print average air quality and the average status
+cursor.execute("""SELECT Status.Description, AVG(Current_AQ_Info.AQI_Value) 
+                    FROM Current_AQ_Info, Status
+                    WHERE Current_AQ_Info.AQI_Value >= Status.Threshold_Lower
+                    AND Current_AQ_Info.AQI_Value <= Status.Threshold_Upper""")
+output = cursor.fetchall()
+#for row in output:
+    #print(row)
+
+#count the amount of times each city had a certain status
+selected_city = 'Tirana'
+cursor.execute("""SELECT Capital_City.Name, Status.Description, COUNT(Status.Description)
+                    FROM History, Status
+                    INNER JOIN Capital_City ON History.City_Key = Capital_City.City_Key
+                    WHERE History.AQI_Value >= Status.Threshold_Lower
+                    AND History.AQI_Value <= Status.Threshold_Upper
+                    GROUP BY Capital_City.Name, Status.Description
+                    ORDER BY Capital_City.Name, Status.Threshold_Lower""")
+output = cursor.fetchall()
+#for row in output:
+    #print(row)
+
+#find cities that had both "Good" and "Moderate" Statuses in its history - change variables if needed
+cursor.execute("""SELECT DISTINCT Capital_City.Name FROM History, Status
+                    INNER JOIN Capital_City ON History.City_Key = Capital_City.City_Key
+                    WHERE History.AQI_Value >= 0
+                    AND History.AQI_Value <= 50
+                  INTERSECT
+                  SELECT DISTINCT Capital_City.Name FROM History, Status
+                    INNER JOIN Capital_City ON History.City_Key = Capital_City.City_Key
+                    WHERE History.AQI_Value >= 51
+                    AND History.AQI_Value <= 100""")
+output = cursor.fetchall()
+#for row in output:
+    #print(row)
+
+#find all cities except for unhealthy or worse cities
+cursor.execute("""SELECT Capital_City.Name FROM Current_AQ_Info
+                    INNER JOIN Capital_City ON Current_AQ_Info.City_Key = Capital_City.City_Key
+                  EXCEPT
+                  SELECT DISTINCT Capital_City.Name FROM Current_AQ_Info, Status
+                    INNER JOIN Capital_City ON Current_AQ_Info.City_Key = Capital_City.City_Key
+                    WHERE Current_AQ_Info.AQI_Value >= 101""")
+output = cursor.fetchall()
+#for row in output:
+    #print(row)
+
+#print all cities that had changes in statuses and order by date
+cursor.execute("""SELECT History.Date, Capital_City.Name, Status.Description
+                    FROM History, Status, Capital_City, (SELECT Capital_City.Name as cities
+                                             FROM History, Status
+                                             INNER JOIN Capital_City ON History.City_Key = Capital_City.City_Key
+                                             WHERE History.AQI_Value >= Status.Threshold_Lower
+                                             AND History.AQI_Value <= Status.Threshold_Upper
+                                             GROUP BY Capital_City.Name
+                                             HAVING COUNT(DISTINCT Status.Description) > 1
+                                             ORDER BY Capital_City.Name, Status.Threshold_Lower) AS cities_with_change
+                    WHERE History.AQI_Value >= Status.Threshold_Lower
+                    AND History.AQI_Value <= Status.Threshold_Upper
+                    AND History.City_Key = Capital_City.City_Key
+                    AND Capital_City.Name = cities_with_change.cities""")
+output = cursor.fetchall()
+#for row in output:
+    #print(row)
 
 # select all countries status
 cursor.execute("""SELECT Country.Name, Status.Description FROM Current_AQ_Info, Status, Capital_City, Country
