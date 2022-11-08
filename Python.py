e9770042f11935320bd2b0721ee045da3a362b0c
Eric Li <49456428+ericli33@users.noreply.github.com>
2022-11-08 14:01:08 -0800

    format change

--- Python.py
+++ Python.py
@@ -453,7 +453,9 @@ db.commit()
 #statement count: 20
 
 #count amount of history entries and user entries
-cursor.execute("SELECT COUNT(History.History_Key) FROM History UNION SELECT COUNT(Users.User_Key) FROM Users")
+cursor.execute("""SELECT COUNT(History.History_Key) FROM History 
+                  UNION 
+                  SELECT COUNT(Users.User_Key) FROM Users""")
 max_user_key = cursor.fetchone()[0]
 max_hist_key = cursor.fetchone()[0]
 #print(max_user_key, max_hist_key)
