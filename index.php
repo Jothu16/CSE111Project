cafd12f0756d0067e11e2dc0c96d472232d8951f
Eric Li <49456428+ericli33@users.noreply.github.com>
2022-12-02 17:09:01 -0800

    fixed php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
 $pdo = new PDO('sqlite:newdb.db');
 
-$statement = $pdo->query("SELECT * FROM newdb");
+$statement = $pdo->query("SELECT * FROM Users");
 
 $rows = $statement->fetchAll(PDO::FETCH_ASSOC);
 
