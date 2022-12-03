e3bdcc80abd3eb3a08acb0e67412a1c806952c61
Eric Li <49456428+ericli33@users.noreply.github.com>
2022-12-03 14:41:45 -0800

    spacing between tables

--- index.php
+++ index.php
@@ -4,14 +4,13 @@ $pdo = new PDO('sqlite:newdb.db');
 
 $statement = $pdo->query("SELECT * FROM Users");
 
-echo "<br>";
-
 $rows = $statement->fetchAll(PDO::FETCH_ASSOC);
 
 var_dump($rows);
 
 $statement = $pdo->query("SELECT * FROM Continent");
 
+echo "<br>";
 echo "<br>";
 
 $rows = $statement->fetchAll(PDO::FETCH_ASSOC);
@@ -21,6 +20,7 @@ var_dump($rows);
 $statement = $pdo->query("SELECT * FROM Country");
 
 echo "<br>";
+echo "<br>";
 
 var_dump($rows);
 
@@ -28,6 +28,7 @@ $rows = $statement->fetchAll(PDO::FETCH_ASSOC);
 
 $statement = $pdo->query("SELECT * FROM Capital_City");
 
+echo "<br>";
 echo "<br>";
 
 var_dump($rows);
@@ -37,6 +38,7 @@ $rows = $statement->fetchAll(PDO::FETCH_ASSOC);
 $statement = $pdo->query("SELECT * FROM History");
 
 echo "<br>";
+echo "<br>";
 
 var_dump($rows);
 
@@ -44,6 +46,7 @@ $rows = $statement->fetchAll(PDO::FETCH_ASSOC);
 
 $statement = $pdo->query("SELECT * FROM Status");
 
+echo "<br>";
 echo "<br>";
 
 var_dump($rows);
@@ -53,6 +56,7 @@ $rows = $statement->fetchAll(PDO::FETCH_ASSOC);
 $statement = $pdo->query("SELECT * FROM UserEdits");
 
 echo "<br>";
+echo "<br>";
 
 var_dump($rows);
 
@@ -61,11 +65,13 @@ $rows = $statement->fetchAll(PDO::FETCH_ASSOC);
 $statement = $pdo->query("SELECT * FROM HistoryStatus");
 
 echo "<br>";
+echo "<br>";
 
 var_dump($rows);
 
 $rows = $statement->fetchAll(PDO::FETCH_ASSOC);
 
+echo "<br>";
 echo "<br>";
 
 var_dump($rows);
@@ -79,9 +85,9 @@ var_dump($rows);
 
 <form action="/action_page.php">
   <label for="fname">First name:</label><br>
-  <input type="text" id="fname" name="fname" value="John"><br>
+  <input type="text" id="fname" name="fname" value="Eric"><br>
   <label for="lname">Last name:</label><br>
-  <input type="text" id="lname" name="lname" value="Doe"><br><br>
+  <input type="text" id="lname" name="lname" value="Li"><br><br>
   <input type="submit" value="Submit">
 </form> 
 
