60d60d00dbbafc8eadcec8e1a351107d8e3bdad5
Harjot Grewal <61030016+Jothu16@users.noreply.github.com>
2022-12-02 16:14:24 -0800

    PHP

--- index.php
+++ index.php
@@ -1,78 +1,9 @@
-<DOCTYPE html>
-   <html lang = 'en'>
-      <head>
-         <meta charset = 'UFT-8'>
-         <title> Air Quality Index Table (AQI) </title>
-</head>
-<body>
-</body>
-   </html>
 <?php
-   class MyDB extends SQLite3 {
-      function __construct() {
-         $this->open('mydb.db');
-      }
-   }
-   $db = new MyDB();
-   if(!$db) {
-      echo $db->lastErrorMsg();
-   } else {
-      echo "Opened database successfully\n";
-   }
 
-   $sql =<<<EOF
-      SELECT * from mytable;
-EOF;
+$pdo = new PDO('sqlite:newdb.db');
 
-   $ret = $db->exec($sql);
-   if(!$ret){
-      echo $db->lastErrorMsg();
-   } else {
-      echo "Table created successfully\n";
-   }
-   $db->close();
-?>
+$statement = $pdo->query("SELECT * FROM newdb");
 
-<!DOCTYPE html>
-<html>
-<body>
-<div class="container">
- <div class="row">
-   <div class="col-sm-8">
-    <?php echo $deleteMsg??''; ?>
-    <div class="table-responsive">
-      <table class="table table-bordered">
-       <thead><tr><th>Date</th>
-         <th>Country</th>
-         <th>Status</th>
-         <th>AQI Value</th>
-    </thead>
-    <tbody>
-  <?php
-      if(is_array($fetchData)){      
-      $sn=1;
-      foreach($fetchData as $data){
-    ?>
-      <tr>
-      <td><?php echo $data['Date']??''; ?></td>
-      <td><?php echo $data['Country']??''; ?></td>
-      <td><?php echo $data['Status']??''; ?></td>
-      <td><?php echo $data['AQI Value']??''; ?></td>  
-     </tr>
-     <?php
-      $sn++;}}else{ ?>
-      <tr>
-        <td colspan="8">
-    <?php echo $fetchData; ?>
-  </td>
-    <tr>
-    <?php
-    }?>
-    </tbody>
-     </table>
-   </div>
-</div>
-</div>
-</div>
-</body>
-</html>
+$rows = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+var_dump($rows);
