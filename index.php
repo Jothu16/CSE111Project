c931348988a5c53325604f9932e506067e51a772
Harjot Grewal <61030016+Jothu16@users.noreply.github.com>
2022-12-03 14:03:11 -0800

    More data on page
    More data on page, adding buttons and text boxes next

--- index.php
+++ index.php
@@ -1,77 +1,61 @@
-<html>
-
 <?php
 
 $pdo = new PDO('sqlite:newdb.db');
 
 $statement = $pdo->query("SELECT * FROM Users");
 
+echo "<br>";
+
+$statement = $pdo->query("SELECT * FROM Continent");
+
+echo "<br>";
+
+$statement = $pdo->query("SELECT * FROM Country");
+
+echo "<br>";
+
+$statement = $pdo->query("SELECT * FROM Capital_City");
+
+echo "<br>";
+
+$statement = $pdo->query("SELECT * FROM History");
+
+echo "<br>";
+
+$statement = $pdo->query("SELECT * FROM Status");
+
+echo "<br>";
+
+$statement = $pdo->query("SELECT * FROM UserEdits");
+
+echo "<br>";
+
+$statement = $pdo->query("SELECT * FROM HistoryStatus");
+
+echo "<br>";
+
 $rows = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+echo "<br>";
 
 var_dump($rows);
 
 ?>
 
-<head>
-<style>
-table {
-  font-family: arial, sans-serif;
-  border-collapse: collapse;
-  width: 100%;
-}
-
-td, th {
-  border: 1px solid #dddddd;
-  text-align: left;
-  padding: 8px;
-}
-
-tr:nth-child(even) {
-  background-color: #dddddd;
-}
-</style>
-</head>
+<html>
 <body>
 
-<h2>HTML Table</h2>
+<h2>HTML Forms</h2>
 
-<table>
-  <tr>
-    <th>Company</th>
-    <th>Contact</th>
-    <th>Country</th>
-  </tr>
-  <tr>
-    <td>Alfreds Futterkiste</td>
-    <td>Maria Anders</td>
-    <td>Germany</td>
-  </tr>
-  <tr>
-    <td>Centro comercial Moctezuma</td>
-    <td>Francisco Chang</td>
-    <td>Mexico</td>
-  </tr>
-  <tr>
-    <td>Ernst Handel</td>
-    <td>Roland Mendel</td>
-    <td>Austria</td>
-  </tr>
-  <tr>
-    <td>Island Trading</td>
-    <td>Helen Bennett</td>
-    <td>UK</td>
-  </tr>
-  <tr>
-    <td>Laughing Bacchus Winecellars</td>
-    <td>Yoshi Tannamuri</td>
-    <td>Canada</td>
-  </tr>
-  <tr>
-    <td>Magazzini Alimentari Riuniti</td>
-    <td>Giovanni Rovelli</td>
-    <td>Italy</td>
-  </tr>
-</table>
+<form action="/action_page.php">
+  <label for="fname">First name:</label><br>
+  <input type="text" id="fname" name="fname" value="John"><br>
+  <label for="lname">Last name:</label><br>
+  <input type="text" id="lname" name="lname" value="Doe"><br><br>
+  <input type="submit" value="Submit">
+</form> 
+
+<p>"/action_page.php".</p>
 
 </body>
 </html>
