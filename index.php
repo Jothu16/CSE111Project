866e3053fed27080d988f2cf66a4709519b57645
Eric Li <49456428+ericli33@users.noreply.github.com>
2022-12-04 14:47:03 -0800

    removed comment block

--- index.php
+++ index.php
@@ -3,24 +3,6 @@
 
 <br>
 <br>
-<!-- <button onclick="countamountofhistoryentries()">count amount of history entries and user entries</button>
-
-<script>
-function countamountofhistoryentries() 
-{
-    var inputId = document.getElementById("HospitalId").value; //we get the user input value and put it in a var
-
-    var xhttp = new XMLHttpRequest();
-    xhttp.open("GET", "C:\Users\kingk\Documents\GitHub\CSE111Project\count_user_history.py" + inputId, true); // we're passing the hId to the server as a parameter
-    xhttp.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-            document.getElementById("SearchBoxPt").value = this.responseText;
-        }
-    };
-    xhttp.send(); 
-
-}
-</script> -->
 
 <form action="../buttons/CountHistoryentries.php" method="get">
    <input type="submit" name="runQ" value="Count History and Users">
