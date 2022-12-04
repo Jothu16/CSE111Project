cc5d09748e5ed82a5a1332383695a0d624d79bc4
Harjot Grewal <61030016+Jothu16@users.noreply.github.com>
2022-12-04 14:59:32 -0800

    Pasted working button, fix on rest now
    Pasted working button format to other files, now have to adjust to the rest of the buttons to work.

--- buttons/UpdateFile.php
+++ buttons/UpdateFile.php
@@ -19,3 +19,35 @@ function updatefile()
 
 }
 </script>
+
+<?php
+    function count_countries_and_capitals() {
+        $db = new SQLite3('../newdb.sqlite');
+
+        $sql = "";
+
+        echo "<table>";
+        echo "<caption>Totals</caption>";
+        echo "<tbody>";
+
+        $ret = $db->query($sql);
+        $row = $ret->fetchArray();
+            
+        echo "<tr>";
+        echo "<td>User Count: " . $row['total'] . "</td>";
+        echo "</tr>";
+
+        $row = $ret->fetchArray();
+            
+        echo "<tr>";
+        echo "<td>History Count: " . $row['total'] . "</td>";
+        echo "</tr>";
+
+        echo "</tbody>";
+        echo "</table>";
+
+        $db->close();
+    }
+count_countries_and_capitals();
+
+?>
