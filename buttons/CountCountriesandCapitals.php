9fb409317feea22250528f10e31a735932678703
Eric Li <49456428+ericli33@users.noreply.github.com>
2022-12-04 15:31:11 -0800

    count country/city working

--- buttons/CountCountriesandCapitals.php
+++ buttons/CountCountriesandCapitals.php
@@ -1,29 +1,10 @@
-<br>
-<br>
-<button onclick="count_amount_countries_and_capitals()">count the amount of countries and their capital cities and their entries(city count = country count since country can only have 1 capital)</button>
-
-<script>
-function count_amount_countries_and_capitals() 
-{
-    var inputId = document.getElementById("HospitalId").value; //we get the user input value and put it in a var
-
-    var xhttp = new XMLHttpRequest();
-    xhttp.open("GET", "C:\Users\kingk\Documents\GitHub\CSE111Project\count_user_history.py" + inputId, true); // we're passing the hId to the server as a parameter
-    xhttp.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-            document.getElementById("SearchBoxPt").value = this.responseText;
-        }
-    };
-    xhttp.send(); 
-
-}
-</script>
-
 <?php
     function count_amount_countries_and_capitals() {
         $db = new SQLite3('../newdb.sqlite');
 
-        $sql = "";
+        $sql = "select COUNT(Capital_City.City_Key) as city_total, COUNT(Country.Country_Key) as country_total " .
+                "FROM Capital_City, Country " .
+                "where Capital_City.Country_Key = Country.Country_Key";
 
         echo "<table>";
         echo "<caption>Totals</caption>";
@@ -33,13 +14,11 @@ function count_amount_countries_and_capitals()
         $row = $ret->fetchArray();
             
         echo "<tr>";
-        echo "<td>User Count: " . $row['total'] . "</td>";
+        echo "<td>Country Total: " . $row['country_total'] . "</td>";
         echo "</tr>";
-
-        $row = $ret->fetchArray();
             
         echo "<tr>";
-        echo "<td>History Count: " . $row['total'] . "</td>";
+        echo "<td>City Total: " . $row['city_total'] . "</td>";
         echo "</tr>";
 
         echo "</tbody>";
--- index.php
+++ index.php
@@ -5,25 +5,9 @@
    <input type="submit" name="runQ" value="Count History and Users">
 </form>
 
-<br>
-<button onclick="countamountofcountriesandcapitals()">count the amount of countries and their capital cities and their entries(city count = country count since country can only have 1 capital)</button>
-
-<script>
-function countamountofcountriesandcapitals() 
-{
-    var inputId = document.getElementById("HospitalId").value; //we get the user input value and put it in a var
-
-    var xhttp = new XMLHttpRequest();
-    xhttp.open("GET", "C:\Users\kingk\Documents\GitHub\CSE111Project\count_user_history.py" + inputId, true); // we're passing the hId to the server as a parameter
-    xhttp.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-            document.getElementById("SearchBoxPt").value = this.responseText;
-        }
-    };
-    xhttp.send(); 
-
-}
-</script>
+<form action="../buttons/CountCountriesandCapitals.php" method="get">
+   <input type="submit" name="runQ" value="Count Cities and Countries">
+</form>
 
 <br>
 <br>
