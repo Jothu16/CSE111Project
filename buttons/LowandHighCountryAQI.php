868a1a7958cfa7a54ed3d482cd99595b6a0cb45b
Harjot Grewal <61030016+Jothu16@users.noreply.github.com>
2022-12-04 15:55:45 -0800

    CountryGoodStatus
    Checking if works

--- buttons/LowandHighCountryAQI.php
+++ buttons/LowandHighCountryAQI.php
@@ -6,24 +6,27 @@
     function Low_and_High_CountryAQI() {
         $db = new SQLite3('../newdb.sqlite');
 
-        $sql = "";
+        $sql = "select * FROM ";
 
         echo "<table>";
-        echo "<caption>Totals</caption>";
-        echo "<tbody>";
+                echo "<caption>Current Air Quality Information</caption>";
+                echo "<thead>";
+                echo "<tr>";
+                echo "<th>AQ_Key</th><th>Date</th><th>AQI_Value</th><th>City_Key</th>";
+                echo "</tr>";
+                echo "</thead>";
+                echo "<tbody>";
+
 
         $ret = $db->query($sql);
-        $row = $ret->fetchArray();
-            
-        echo "<tr>";
-        echo "<td>User Count: " . $row['total'] . "</td>";
-        echo "</tr>";
-
-        $row = $ret->fetchArray();
-            
-        echo "<tr>";
-        echo "<td>History Count: " . $row['total'] . "</td>";
-        echo "</tr>";
+        while ($row = $ret->fetchArray()) {
+            echo "<tr>";
+            echo "<td>" . $row["AQ_Key"] . "</td>" .
+                "<td>" . $row["Date"] . "</td>" .
+                "<td>" . $row["AQI_Value"] . "</td>" .
+                "<td>" . $row["City_Key"] . "</td>";
+            echo "</tr>";
+        }
 
         echo "</tbody>";
         echo "</table>";
