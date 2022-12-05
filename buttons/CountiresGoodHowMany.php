11f2e3d288ca1d2e44ce6607c63a45cbc7862f15
Harjot Grewal <61030016+Jothu16@users.noreply.github.com>
2022-12-04 16:18:44 -0800

    CountriesGoodHowMany
    Updated sql statement

--- buttons/CountiresGoodHowMany.php
+++ buttons/CountiresGoodHowMany.php
@@ -6,7 +6,16 @@
     function Continents_withCountry_Good_HowMany() {
         $db = new SQLite3('../newdb.sqlite');
 
-        $sql = "";
+        $sql = "SELECT Continent.Name, COUNT(Country.Name) FROM Current_AQ_Info
+        INNER JOIN Capital_City ON Current_AQ_Info.City_Key = Capital_City.City_Key
+        INNER JOIN Country ON Capital_City.Country_Key = Country.Country_Key
+        INNER JOIN Continent ON Country.Cont_Key = Continent.Cont_Key
+        INNER JOIN (SELECT DISTINCT Capital_City.Name AS good_ones FROM Current_AQ_Info, Status
+                            INNER JOIN Capital_City ON Current_AQ_Info.City_Key = Capital_City.City_Key
+                            WHERE Current_AQ_Info.AQI_Value >= 0
+                            AND Current_AQ_Info.AQI_Value <= 50)
+             AS good_cities ON good_cities.good_ones = Capital_City.Name
+        GROUP BY Continent.Name";
 
         echo "<table>";
         echo "<caption>Totals</caption>";
